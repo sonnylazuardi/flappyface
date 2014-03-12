894b0618952e2b3b18273f68723d7ba019b49bd3
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-03-12 15:28:03 +0700

    test

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 80);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
